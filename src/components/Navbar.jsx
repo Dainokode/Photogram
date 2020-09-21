52242d8b901ededc581aecc3ac0d208caad3818c
Antonio Daino <64027381+Dainokode@users.noreply.github.com>
2020-09-21 10:18:03 +0200

    Add paragraph, font family and more styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav id="navbar">
-      <h1 className="logo">Photogram</h1>
-    </nav>
+    <>
+      <nav id="navbar">
+        <h1 className="logo">Photogram</h1>
+      </nav>
+      <div className="showcase">
+        <h2>Your favorite pictures</h2>
+        <p>Find or upload your favorite pictures...</p>
+      </div>
+    </>
   );
 };
 
