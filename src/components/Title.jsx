6af905fddd9d6365e5bf77f1afdb78f7f53850b0
Antonio Daino <64027381+Dainokode@users.noreply.github.com>
2020-09-21 09:43:30 +0200

    Add Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Photogram</h1>
+      <h2>Your favorite pictures</h2>
+    </div>
+  );
+};
+
+export default Title;
