030e4057623bfbc15629c40d55775ed927454c73
Antonio Daino <64027381+Dainokode@users.noreply.github.com>
2020-09-22 11:28:02 +0200

    Set states (error and file) of the app to show error or image name correctly

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  // allowed types of images
+  const types = ["image/png", "image/jpeg"];
+
+  const handleChange = (e) => {
+    const selectedFile = e.target.files[0];
+
+    if (selectedFile && types.includes(selectedFile.type)) {
+      setFile(selectedFile);
+      setError("");
+    } else {
+      setFile(null);
+      setError("Please select a png or jpeg file");
+    }
+  };
+
   return (
     <form>
-      <input type="file" />
+      <input type="file" onChange={handleChange} />
+      {error ? <div className="error">{error}</div> : null}
+      {file ? <div className="file">{file.name}</div> : null}
     </form>
   );
 };
